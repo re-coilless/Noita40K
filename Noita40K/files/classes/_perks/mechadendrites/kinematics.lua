e0b5e885ea21d6670037cb432119d21c0aa02fb9
Recoilless Interactive <68288735+re-coilless@users.noreply.github.com>
2026-06-30 18:05:36 +0400

    incomprehensible

--- Noita40K/files/classes/_perks/mechadendrites/kinematics.lua
+++ Noita40K/files/classes/_perks/mechadendrites/kinematics.lua
@@ -2,90 +2,60 @@ dofile_once( "mods/Noita40K/files/_lib.lua" )
 
 local limb_id = GetUpdatedEntityID()
 local hooman = EntityGetRootEntity( limb_id )
---local center_x, center_y = pen.get_creature_centre( hooman )
 local base_x, base_y, base_r, base_s_x, base_s_y = EntityGetTransform( limb_id )
---base_y = base_y + center_y/2
-
---fully procedural, put this in penman
-
 local is_active = pen.magic_storage( limb_id, "is_active", "value_bool" )
-
-local t_x = base_x - 7*base_s_x
-local t_y = base_y
-
+local t_x, t_y = base_x - 7*base_s_x, base_y + 1
 local accuracy = 8
+
 if( is_active ) then
 	local length = pen.magic_storage( limb_id, "length", "value_float" )
 	local temp_x = pen.magic_storage( limb_id, "target_x", "value_float" )
 	local temp_y = pen.magic_storage( limb_id, "target_y", "value_float" )
-	if(( temp_x == 0 and temp_y == 0 ) or math.sqrt(( base_x - temp_x )^2 + ( base_y - temp_y )^2 ) > length ) then
-		is_active = false
-	else t_x, t_y, accuracy = temp_x, temp_y, accuracy/2 end
+	temp_x, temp_y = pen.get_mouse_pos( true )
+	if( pen.magic_storage( limb_id, "offset_mode", "value_bool" )) then
+		temp_x, temp_y = base_x + temp_x, base_y + temp_y end
+	if(( temp_x ~= 0 or temp_y ~= 0 ) and math.sqrt(( base_x - temp_x )^2 + ( base_y - temp_y )^2 ) < length ) then
+		t_x, t_y, accuracy = temp_x, temp_y, accuracy/2
+	else is_active = false end
 end
 
-local first_link = 12
-local first_limit = 6
-local second_link = 2
-local third_link = 14
-local third_limit = 8
-
-local stretching_k = 1/( first_limit + third_limit )
-local stretching = first_link + first_limit + second_link + third_link + third_limit
+local lmt_1A, lmt_1B, lmt_15, lmt_2A, lmt_2B = unpack(
+	pen.t.pack( pen.magic_storage( limb_id, "limits", "value_string" )))
+local stretching = lmt_1A + lmt_1B + lmt_15 + lmt_2A + lmt_2B
+local stretching_k = 1/( lmt_1B + lmt_2B )
 
 local c_x, c_y = 0, 0
 local children = EntityGetAllChildren( limb_id ) or {}
-if( #children > 0 ) then
-	for i,d_module in ipairs( children ) do
-		if( EntityGetName( d_module ) == "claw" ) then
-			c_x, c_y = EntityGetTransform( d_module )
-			break
-		end
+for i,d_module in ipairs( children ) do
+	if( EntityGetName( d_module ) == "claw" ) then
+		c_x, c_y = EntityGetTransform( d_module )
+		break
 	end
 end
 
 local speed_k = 2.5
-local d_x = t_x - c_x
-local d_y = t_y - c_y
+local d_x, d_y = t_x - c_x, t_y - c_y
 if( math.sqrt( d_x^2 + d_y^2 ) > accuracy ) then
 	t_x = c_x + pen.sgn( d_x )*speed_k*math.log( math.abs( d_x ) + 1 )
 	t_y = c_y + pen.sgn( d_y )*speed_k*math.log( math.abs( d_y ) + 1 )
 else pen.magic_storage( limb_id, "is_going", "value_bool", false ) end
-t_x = t_x - base_x
-t_y = t_y - base_y
+t_x, t_y = t_x - base_x, t_y - base_y
 
-local length = math.sqrt( t_x^2 + t_y^2 )
+local h, a, b = 0, 0, 0
 local angle = -math.atan2( t_y, t_x )
+local length = math.sqrt( t_x^2 + t_y^2 )
+if( length + 2 >= lmt_1A + lmt_15 + lmt_2A ) then
+	length = math.min( length, stretching )
+	stretching = length - ( lmt_1A + lmt_15 + lmt_2A ) --subtracting from this causes the change in final angle
+	lmt_1B = lmt_1B*stretching*stretching_k + 3
+	lmt_2B = lmt_2B*stretching*stretching_k + 2
+else lmt_1B, lmt_2B = 2, 1 end
 
-local h = 0
-local a = 0
-local b = 0
-
-if( length + 2 >= first_link + second_link + third_link ) then
-	if( length > stretching ) then
-		length = stretching
-	end
-	stretching = length - ( first_link + second_link + third_link )
-	first_limit = first_limit*stretching*stretching_k + 2
-	third_limit = third_limit*stretching*stretching_k + 3
-else
-	first_limit = 1
-	third_limit = 2
-end
-
-local tmp_first = first_link + first_limit
-local tmp_third = third_link + third_limit
-h = math.sqrt( tmp_first^2 - ((( length - second_link )^2 + tmp_first^2 - tmp_third^2 )/( 2*( length - second_link )) )^2 )
-if( h < tmp_first ) then
-	a = math.sqrt( tmp_first^2 - h^2 )
-else
-	h = tmp_first
-end
-if( not( is_active ) ) then
-	h = -pen.sgn( t_x )*h
-else
-	h = -pen.sgn( t_x )*pen.sgn( t_y )*h
-end
-b = a + second_link
+local tmp_first, tmp_third = lmt_1A + lmt_1B, lmt_2A + lmt_2B
+h = math.sqrt( tmp_first^2 - ((( length - lmt_15 )^2 + tmp_first^2 - tmp_third^2 )/( 2*( length - lmt_15 )) )^2 )
+if( h < tmp_first ) then a = math.sqrt( tmp_first^2 - h^2 ) else h = tmp_first end
+if( is_active ) then h = -pen.sgn( t_x )*pen.sgn( t_y )*h else h = -pen.sgn( t_x )*h end
+b = a + lmt_15
 
 local dx1 = a*math.cos( angle ) + h*math.sin( angle )
 local dy1 = -a*math.sin( angle ) + h*math.cos( angle )
@@ -96,15 +66,13 @@ local angle_back = math.atan2( dy1, dx1 )
 local angle_front = math.atan2( t_y - dy2, t_x - dx2 )
 local pos = {
 	{ base_x, base_y, angle_back },
-	{ base_x + math.cos( angle_back )*first_limit, base_y + math.sin( angle_back )*first_limit, angle_back },
+	{ base_x + math.cos( angle_back )*lmt_1B, base_y + math.sin( angle_back )*lmt_1B, angle_back },
 	{ base_x + dx1, base_y + dy1, -angle },
 	{ base_x + dx2, base_y + dy2, angle_front },
-	{ base_x + dx2 + math.cos( angle_front )*third_limit, base_y + dy2 + math.sin( angle_front )*third_limit, angle_front },
+	{ base_x + dx2 + math.cos( angle_front )*lmt_2B, base_y + dy2 + math.sin( angle_front )*lmt_2B, angle_front },
 	{ base_x + dx2 + math.cos( angle_front )*( tmp_third - 2 ), base_y + dy2 + math.sin( angle_front )*( tmp_third - 2 ), angle_front },
 }
 
-if( #children > 0 ) then
-	for i,d_module in ipairs( children ) do
-		EntitySetTransform( d_module, pos[i][1], pos[i][2], pos[i][3], 1, 1 )
-	end
+for i,d_module in ipairs( children ) do
+	EntitySetTransform( d_module, pos[i][1], pos[i][2], pos[i][3], 1, 1 )
 end
